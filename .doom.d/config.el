c68e4b55b9e38024e147cd0688267c18098bfb67
Eugene <me@aryon.dev>
2022-03-24 20:05:28 +0400

    update doom config: enable lsp, blamer and docker

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -4,26 +4,54 @@
       user-mail-address "me@aryon.dev"
 
       doom-font (font-spec :family "Fantasque Sans Mono" :size 16)
+      doom-theme 'doom-molokai
+      
+      org-directory "~/org/"
 
-      doom-theme 'doom-gruvbox
-      doom-themes-treemacs-theme 'doom-treemacs-colors
+      display-line-numbers-type 'relative
 
-      projectile-project-search-path "~/Code"
+      projectile-project-search-path '("~/Code")
 
-      ;; мимикрируем под нормальных людей
+      rspec-use-docker-when-possible t
+      rspec-docker-command "docker compose run"
+      rspec-docker-container "runner"
+
       elcord--editor-name "Emacs"
-
-      elcord--editor-icon "emacs_pen_icon"
-
       elcord--editor-icon "emacs_pen_icon"
       elcord-use-major-mode-as-main-icon t
-      )
 
-(require 'ace-window)
+      blamer-commit-formatter "- %s"
+      blamer-max-commit-message-length 40
 
-;; make treemacs selectable from ace-window
-(after! treemacs
-  (setq aw-ignored-buffers (delq 'treemacs-mode aw-ignored-buffers)))
+      lsp-ui-doc-show-with-cursor t
+      lsp-ui-doc-show-with-mouse t)
+
+(map! :leader
+      :desc "robe-jump (Go to definition)"
+      "m g g" #'robe-jump)
+
+;; fix company "do -> downcase" in ruby-mode
+(setq company-minimum-prefix-length 3)
+
+(use-package blamer
+  :defer 20
+  :custom
+  (blamer-idle-time 0.3)
+  (blamer-min-offset 20)
+  :custom-face
+  (blamer-face ((t :foreground "#928374"
+                   :background nil
+                   :height 140
+                   :italic t)))
+  :config
+  (global-blamer-mode 1))
+
+;; custom functions...
+(defun yml-sorter ()
+  "Run 'yml-sorter' on file from current buffer"
+  (interactive)
+  (shell-command (concat "yml-sorter --input " buffer-file-name))
+  )
 
 ;; fix cyrillic fonts
 (after! unicode-fonts
@@ -42,15 +70,4 @@
 
     ))
 
-;; fix annoying company's "do -> downcase" in ruby-mode
-(setq company-minimum-prefix-length 3)
-
 (elcord-mode)
-
-
-;; custom functions...
-(defun yml-sorter ()
-  "Run `yml-sorter' on file from current buffer"
-  (interactive)
-  (shell-command (concat "yml-sorter --input " buffer-file-name))
-  )
--- .doom.d/packages.el
+++ .doom.d/packages.el
@@ -5,3 +5,12 @@
 
 (package! docker-robe
   :recipe (:host github :repo "aki2o/emacs-docker-robe" ))
+
+(package! blamer
+  :recipe (:host github :repo "artawower/blamer.el"))
+
+(package! plz
+  :recipe (:host github :repo "alphapapa/plz.el"))
+
+(package! ement
+  :recipe (:host github :repo "alphapapa/ement.el"))
